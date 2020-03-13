0be6f1cfe0c6bdb720c3eb9c5ba28d715e96c99d
sinnkami <sinnkami@users.noreply.github.com>
2020-03-13 09:56:38 +0900

    bin/www のts対応

---
+++ app/bin/www.ts
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+
+import app from "../app";
+import http from "http";
+import config from "config";
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(config.get("port"));
+app.set("port", port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val: string): number {
+	const port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return 0;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return 0;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error: { syscall: string; code: string }): void {
+	if (error.syscall !== "listen") {
+		throw error;
+	}
+
+	const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+		case "EACCES":
+			console.error(bind + " requires elevated privileges");
+			process.exit(1);
+			break;
+		case "EADDRINUSE":
+			console.error(bind + " is already in use");
+			process.exit(1);
+			break;
+		default:
+			throw error;
+	}
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening(): void {
+	const addr = server.address();
+	if (!addr) return;
+	const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+	console.debug("Listening on " + bind);
+}
