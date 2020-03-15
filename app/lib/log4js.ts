ada18adcd1d894f0e53cb170b2496948d43d6775
sinnkami <sinnkami@users.noreply.github.com>
2020-03-15 17:56:26 +0900

    不要なログ

--- app/lib/log4js.ts
+++ app/lib/log4js.ts
@@ -1,7 +1,6 @@
 import log4js, { Configuration } from "log4js";
 import config from "config";
 
-console.log(log4js);
 if (config.has("logger") && config.get("logger")) {
 	const configure: Configuration = config.get("logger.configure");
 	log4js.configure(configure);
