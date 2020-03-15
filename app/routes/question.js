bfbab4e5ff94fb9542f15d9d6d3f7be07772b77c
sinnkami <sinnkami@users.noreply.github.com>
2020-03-15 18:16:11 +0900

    router/questionのts対応

--- app/routes/question.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    next("NotImplemented");
-});
-
-module.exports = router;
