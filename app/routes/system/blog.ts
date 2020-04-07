98c71aa54e7cc07f8c95385333f3c9d58a08aaa1
sinnkami <sinnkami@users.noreply.github.com>
2020-04-07 17:46:41 +0900

    記事一覧を取得できる様に

--- app/routes/system/blog.ts
+++ app/routes/system/blog.ts
@@ -1,9 +1,29 @@
 import express from "express";
+import log4js from "../../lib/modules/log4js";
+const logger = log4js.getLogger();
+
+import EntryService from "../../lib/service/EntryService";
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res) {
-	res.render("pages/system/blog");
+router.get("/", function(req, res, next) {
+	const page = Number(req.query.page) || 1;
+	EntryService.getEntries(page)
+		.then(function(value) {
+			res.render("pages/system/blog", {
+				contents: value.entryList,
+				currentPage: page,
+				maxPage: Math.ceil(value.entryCount / 10),
+			});
+		})
+		.catch(function(err) {
+			logger.error(err);
+			next("NotFound");
+		});
+});
+
+router.get("/edit", function(req, res) {
+	res.render("pages/system/edit");
 });
 
 export = router;
