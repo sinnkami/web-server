051194bd429817c71dc809509c5eccc0986be7a2
sinnkami <sinnkami@users.noreply.github.com>
2020-08-06 20:11:36 +0900

    envを指定して鯖を起動する様に変更

--- gulp/00_server.js
+++ gulp/00_server.js
@@ -2,9 +2,9 @@ const gulp = require("gulp");
 const args = require("./lib/value");
 
 const gls = require("gulp-live-server");
-const server = gls(`${args.dest}/bin/www`, undefined, false);
+const server = gls(`${args.dest}/bin/www`, { env: { NODE_ENV: 'test' } }, false);
 
-gulp.task("server", function() {
+gulp.task("server", function () {
 	if (server) {
 		server.stop();
 	}
