e93de64aeb035e0248634b6d13780da4704cda0f
sinnkami <sinnkami@users.noreply.github.com>
2020-08-12 17:46:39 +0900

    sourcemap を追加した

--- gulp/00_typescript.js
+++ gulp/00_typescript.js
@@ -5,16 +5,34 @@ const babel = require("gulp-babel");
 const eslint = require("gulp-eslint");
 const plumber = require("gulp-plumber");
 const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task("typescript:normal", function () {
 	return gulp
 		.src([`${args.src}/**/*.ts`, `!${args.src}/**/*.min.*`, `!${args.src}/node_modules/**/*`])
 		.pipe(plumber())
+		.pipe(sourcemaps.init())
 		.pipe(eslint({ useEslintrc: true })) // .eslintrc を参照
 		.pipe(eslint.format())
-		.pipe(tsProject())
+		.pipe(ts({
+			"target": "ES6",
+			"module": "commonjs",
+			"allowJs": false,
+			"declaration": false,
+			"declarationMap": false,
+			"rootDir": "./app",
+			"removeComments": true,
+			"noEmit": false,
+			"isolatedModules": false,
+
+			"esModuleInterop": true,
+
+			"strict": true,
+
+			"forceConsistentCasingInFileNames": true
+		}))
 		.pipe(babel())
+		.pipe(sourcemaps.write("maps/"))//, { includeContent: false, sourceRoot: '../app/' }
 		.pipe(gulp.dest(`${args.dest}/`));
 });
 
--- gulp/02_watch.js
+++ gulp/02_watch.js
@@ -8,10 +8,8 @@ gulp.task("watch:express", callback => {
 		return callback();
 	}
 
-	gulp.task("server").call();
-
-	gulp.watch(`${args.src}/package.json`, gulp.series("package", "server"));
+	gulp.watch(`${args.src}/package.json`, gulp.series("package"));
 	gulp.watch(`${args.src}/public/**/*`, gulp.series("public"));
 	gulp.watch(`${args.src}/views/**/*`, gulp.series("views"));
-	gulp.watch(`${args.src}/**/*.ts`, gulp.series("typescript", "server"));
+	gulp.watch(`${args.src}/**/*.ts`, gulp.series("typescript"));
 });
